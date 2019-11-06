ea9191484093fd96d4ca1c5565cc36e2ebc8ba86
Mihas <32958803+MrMihas@users.noreply.github.com>
2019-11-06 09:51:33 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,25 +1,27 @@
-
-
-
 $(document).ready(function() {
   $('.btn-description').click(function() {
     $('.description').removeClass('description__active');
     if ($(this).next('.description').css("display") == "none") {
+		 
       $('.description').hide('normal');
       $(this).next('.description').toggle('normal');
 
-    //  $('.btn-description').removeClass('btn-description__active span:nth-child(1)');
-    //  $(this).toggleClass('btn-description__active span:nth-child(1)');
+		 
+      $('.btn-description').removeClass('btn-description__active span:nth-child(1)');
+      $(this).toggleClass('btn-description__active span:nth-child(1)');
+		 
 
-      
       $('.btn-description').prev().removeClass('name-project__active');
       $(this).prev().toggleClass('name-project__active');
 
-    } else $('.description').hide('normal');
-    return false;
+    } else {
+		 $('.description').hide('normal');
+		 $('.name-project').removeClass('name-project__active');
+		 $('.btn-description').removeClass('btn-description__active span:nth-child(1)');
+	 }
+    
   });
 });
-
 
 
 
